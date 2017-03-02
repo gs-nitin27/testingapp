e82d4a67a06f1c5c6b5d54a0b27cd4534fd3b737
devendra <devendrakumarpandey@gmail.com>
2017-03-02 18:13:48 +0530

    coach student

--- connect_user.php
+++ connect_user.php
@@ -17,14 +17,8 @@ if($_REQUEST['act'] == 'connect')
     $prof_id   = $request_user['prof_id'];
     $user_data = $userdata->getuserdata($user_responser_id);
     $device_id = $user_data['device_id'];
-
-    // $array_data = array('connection_id' => $connection_id, 'title'=> 'New Connection Request', 'msg'=> $name.' wants to connect with you' , 'device_id' => $device_id);
-    // $json_data = json_encode($array_data);
-
     $array_data = array('connection_id' => $connection_id,'lite_user_id' => $user_request_id, 'prof_id' => $prof_id, 'title'=> 'New Connection Request', 'message'=> $name.' wants to connect with you' , 'device_id' => $device_id , 'indicator' => 1);
     $json_data = json_encode($array_data);
-
-
     if($connection_id)
     {
      $notification = $userdata->sendPushNotificationToGCM($device_id,$array_data);
@@ -40,31 +34,33 @@ if($_REQUEST['act'] == 'connect')
     }
 }
 
+
+
  else if($_REQUEST['act'] == 'request_response')
  { 
-   //$data =  json_decode($_REQUEST['data']); 
+
    $request_id  = $_REQUEST['id'];
    $req_status  = $_REQUEST['req_status'];
    $user_app    = $_REQUEST['user_app'];
-   
    $req = new connect_userservice();
    $res = $req->connect_user_response($request_id , $req_status);
    if($res == 1)
    {
-         $user_id =  $req->getuserid($request_id);
-         $userid = $user_id['lite_user_id'];
-         $userdata = new userdataservice();
-         $request_user = $userdata->getuserdata($userid);
-         $response_user = $userdata->getuserdata($user_id['prof_user_id']);
+         $user_id       =  $req->getuserid($request_id);
+         $lite_user_id  =$user_id[0]['lite_user_id'];
+         $userdata      = new userdataservice();
+         $request_user  = $userdata->getuserdata($lite_user_id);
+         $response_user  = $userdata->getuserdata($user_id[0]['prof_user_id']);
          $response_user_name = $response_user['name'];
          $device_id = $request_user['device_id'];
          $array_data = array('title'=> 'New Connection ', 'message'=> $response_user_name.' is connected with you' , 'device_id' => $device_id , 'indicator' =>2);
          $json_data = json_encode($array_data);
          $notification = $userdata->sendLitePushNotificationToGCM($device_id,$array_data);
-         $alerts =$req->alerts($userid ,$user_app , $json_data) ;
+         $alerts =$req->alerts($lite_user_id ,$user_app , $json_data) ;
          $message_seen = $req->updateseennotification($alerts);
          $user = array('status' => 1, 'message'=>'User Connected' );
          echo json_encode($user);
+
 
    }
    else if($res == 2)
@@ -82,6 +78,10 @@ if($_REQUEST['act'] == 'connect')
      $user = array('status' => 0, 'message' =>'Request is not cancelled');
    }
 }
+
+
+
+
 
 
 
