d0fc38e8bac8bcd7457a62f44a2cde99cdeb7711
sagar seth <sagar12jan@gmail.com>
2017-04-26 13:10:07 +0530

    userjob details update

--- services/userdataservice.php
+++ services/userdataservice.php
@@ -1537,7 +1537,7 @@ public function getAppliedJobListing($userid,$jobid)
 {
 $n=1;
 
-$query =mysql_query("SELECT ji.`userid` AS employerid, ji.`id` AS job_id, uj.`userid` AS applicant_id ,us.`name` AS applicant_name ,us.`user_image` AS applicant_image ,us.`prof_name` AS prof_name ,  us.`prof_id` AS prof_id, us.`sport` AS sport,uj.`status` As status FROM `gs_jobInfo` AS ji LEFT JOIN `user_jobs` AS uj ON ji.`id` = uj.`userjob` LEFT JOIN `user` AS us ON us.`userid` = uj.`userid` WHERE ji.`userid`='$userid' AND ji.`id`='$jobid' AND uj.`status`>='$n'");
+$query =mysql_query("SELECT ji.`userid` AS employerid, ji.`id` AS job_id, uj.`userid` AS applicant_id ,us.`name` AS applicant_name ,us.`user_image` AS applicant_image ,us.`prof_name` AS prof_name ,  us.`prof_id` AS prof_id, us.`sport` AS sport, us.`dob` AS dob, us.`locaton` AS user_location , uj.`status` As status FROM `gs_jobInfo` AS ji LEFT JOIN `user_jobs` AS uj ON ji.`id` = uj.`userjob` LEFT JOIN `user` AS us ON us.`userid` = uj.`userid` WHERE ji.`userid`='$userid' AND ji.`id`='$jobid' AND uj.`status`>='$n'");
 if(mysql_num_rows($query)>0)
 {
 while($row = mysql_fetch_assoc($query))
