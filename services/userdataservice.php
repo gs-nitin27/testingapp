afb0a29e9edc94b68fd73fade915f8410a28a269
sagar seth <sagar12jan@gmail.com>
2017-04-26 13:18:45 +0530

    bug fix

--- services/userdataservice.php
+++ services/userdataservice.php
@@ -1542,6 +1542,14 @@ if(mysql_num_rows($query)>0)
 {
 while($row = mysql_fetch_assoc($query))
 {
+                       $date_1 = new DateTime($row['dob']);
+                       $date_2 = new DateTime( date( 'd-m-Y' ));
+                       $difference = $date_2->diff( $date_1 );
+                       $year=(string)$difference->y;
+
+
+
+                       $row['age'] = $year;
 $rows[] = $row;
 }
 return $rows;
