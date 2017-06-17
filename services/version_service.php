99d4181d9e189adb3637fc16b3ce0da211be5dfe
dev538623 <devendra@darkhorsesports.in>
2017-06-17 16:14:05 +0530

    bug fix

--- services/version_service.php
+++ services/version_service.php
@@ -1,11 +1,11 @@
 <?php
 class version_service
 {
-	public function cheack_version($version_key)
+	public function cheack_version($version_key,$app_type)
 	{
-		$query = mysql_query("SELECT *FROM `gs_version` WHERE `version_key`=$version_key ");
+		$query = mysql_query("SELECT *FROM `gs_version` WHERE `version_key`>=$version_key AND `app_type`='$app_type'");
 		$num = mysql_num_rows($query);
-		if ($num)
+		if($num)
 		{
 			return 1;
 		}
--- version_controller.php
+++ version_controller.php
@@ -4,21 +4,25 @@ include('services/version_service.php');
 
   if($_REQUEST['act'] == 'app_version')
   { 
-  $data              =  file_get_contents("php://input");
-  $data1             =  json_decode(file_get_contents("php://input"));
-  $version_key       =  $data1->version_key ;
+  $data              =   file_get_contents("php://input");
+  $data1             =   json_decode(file_get_contents("php://input"));
+  $version_key       =   $data1->version_key ;
+  $app_type          =   $data1->app_type ;
   $request           =   new version_service();
-  $response          =   $request->cheack_version($version_key);
-  if($response)
+  $response          =   $request->cheack_version($version_key,$app_type);
+  if($response==1)
    {
-   		$sucess  = array('forceUpgrade'=>'true','recommondUpgrade'=>'true','stayonApp'=>"true");
-   		$Result = array('status' => '1','data'=>[$sucess],'msg'=>'Updated');
+
+          $sucess   = array('forceUpgrade'=>'false','recommondUpgrade'=>'false','stayonApp'=>"false"); 
+          $Result = array('status' => '0','data'=>[$sucess] ,'msg'=>'No Updated');
           echo json_encode($Result);
+
+
    }
    else
    {        
-          $fail   = array('forceUpgrade'=>'false','recommondUpgrade'=>'false','stayonApp'=>"false"); 
-          $Result = array('status' => '0','data'=>[$fail] ,'msg'=>'No Updated');
+      $fail   = array('forceUpgrade'=>'true','recommondUpgrade'=>'true','stayonApp'=>"true");
+      $Result = array('status' => '1','data'=>[$fail],'msg'=>'Updated');
           echo json_encode($Result);
    } 
 }
