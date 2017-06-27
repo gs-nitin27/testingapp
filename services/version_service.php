e0e1ed07b963faee7f0527688c504136802d38ae
gs-server <nitin@darkhorsesports.in>
2017-06-27 19:17:47 +0530

    version big fixing

--- services/version_service.php
+++ services/version_service.php
@@ -3,7 +3,7 @@ class version_service
 {
 	public function cheack_version($version_key,$app_type)
 	{
-		$query = mysql_query("SELECT *FROM `gs_version` WHERE `version_key`<> $version_key AND `app_type`='$app_type'");
+		$query = mysql_query("SELECT *FROM `gs_version` WHERE `version_key` > $version_key AND `app_type`='$app_type'");
 		$num = mysql_num_rows($query);
   		if($num)
 		{
