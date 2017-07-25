18903d6fd92b83e9cf6e26772118f3a7eb2f647b
nitin <nitin@darkhorsesports.in>
2017-07-25 14:34:48 +0530

    test api commit

--- ManageSchedule_classController.php
+++ ManageSchedule_classController.php
@@ -8,39 +8,37 @@ error_reporting(E_ERROR | E_PARSE);
 
 if($_REQUEST['act'] == "create_class")
 {
-	//print_r("expression");die;
-							$data  = json_decode($_POST['data']);
-							//print_r($data);
-							$item  = new stdClass();
+    $data  = json_decode(file_get_contents("php://input"));
+	$item  = new stdClass();
 
-                            $date = date_create($data->start_date);
-                            date_add($date, date_interval_create_from_date_string($data->duration.'months'));
-                            $ndate = date_format($date, 'Y-m-d');
+    $date = date_create($data->start_date);
+    date_add($date, date_interval_create_from_date_string($data->duration.'months'));
+    $ndate = date_format($date, 'Y-m-d');
 
-							$item->class_name      = $data->class_name;
-							$item->description     = $data->description;
-							$item->days            = $data->days;
-							$item->duration        = $data->duration;
-							$item->start_date      = strtotime($data->start_date);
-							$item->end_date        = strtotime($ndate);
-							$item->start_time      = $data->start_time;
-							$item->end_time        = $data->end_time;
-							$item->address         = $data->address;
-							$item->user_id         = $data->user_id;
-							$item->location        = $data->location;
-							$item->fee             = $data->payment;
-							$item->age_group       = $data->age_group;
-							$item->class_strength  = $data->class_strength;
-							$item->class_host      = $data->class_host;
-							$item->phone_no        = $data->contact_no;
-							$item->classtype       = $data->classtype;
+	$item->class_name      = $data->class_name;
+	$item->description     = $data->description;
+	$item->days            = $data->days;
+	$item->duration        = $data->duration;
+	$item->start_date      = strtotime($data->start_date);
+	$item->end_date        = strtotime($ndate);
+	$item->start_time      = $data->start_time;
+	$item->end_time        = $data->end_time;
+	$item->address         = $data->address;
+	$item->user_id         = $data->user_id;
+	$item->location        = $data->location;
+	$item->fee             = $data->payment;
+	$item->age_group       = $data->age_group;
+	$item->class_strength  = $data->class_strength;
+	$item->class_host      = $data->class_host;
+	$item->phone_no        = $data->contact_no;
+	$item->classtype       = $data->classtype;
 
-                              //print_r($data->contact_no);die;
+      //print_r($data->contact_no);die;
 
-                        $code = $item->user_id.'@'.substr(str_replace(' ','', $item->start_time),0,3).substr($data->start_date, 3,2).substr($data->start_date,8,2);
-                            
-                            $req2 = new manageSchedulingService();
-                            $res2 = $req2->CheckforExistingClass($item);
+    $code = $item->user_id.'@'.substr(str_replace(' ','', $item->start_time),0,3).substr($data->start_date, 3,2).substr($data->start_date,8,2);
+    
+    $req2 = new manageSchedulingService();
+    $res2 = $req2->CheckforExistingClass($item);
               if($res2 == 1)
             {
 					    
