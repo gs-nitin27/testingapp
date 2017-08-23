a0e105558d8747cc74af16ddc77db690da34c973
dev538623 <devendra@darkhorsesports.in>
2017-08-23 13:16:55 +0530

    bug fix

--- services/UserProfileService.php
+++ services/UserProfileService.php
@@ -598,68 +598,6 @@ public function edit_profile($userdata)
 
 
 
-public function diet_plan($userdata,$userid)
-{
-
-   $query = mysql_query("INSERT INTO `gs_diet_plan`(`userid`, `my_diet_plan`,`date_created`,`date_updated`) VALUES ($userid,'$userdata',CURDATE(), CURDATE()) ON DUPLICATE KEY UPDATE `my_diet_plan`= '$userdata',`date_updated` = CURDATE()");
-   if($query)
-    {
-        return 1;
-    } 
-    else
-    {    
-        return 0;
-    } 
-
-
-
-
-}
-
-
-
-
-
-public function list_plan($userid)
-{
- $query=mysql_query("SELECT `my_diet_plan`, `id`  FROM `gs_diet_plan` WHERE `userid`='$userid'");
-  if(mysql_num_rows($query)>0)
-       {
-          while($row = mysql_fetch_assoc($query))
-          {
-            $row['my_diet_plan']  =  json_decode($row['my_diet_plan']);
-            $my_diet_plan1        =  (array)$row['my_diet_plan'];
-            $my_diet_plan1['id']  =  $row['id'];
-            $data[]               =  $my_diet_plan1;
-          }
-          return $data;
-          }
-          else 
-          {
-          return 0;
-          }
-}
-
-
-
-
-
-
-public function edit_plan($id,$my_diet_plan)
-{
-  $query  = mysql_query("UPDATE `gs_diet_plan` SET `my_diet_plan`='$my_diet_plan' WHERE `id` ='$id' ");
-  $num = mysql_affected_rows();
-  if($num)
-  {
-      return 1;
-  }
-  else 
-  {
-     return 0;
-  }
-
-}
-
 
 } // End of Class
 
--- userEdit.php
+++ userEdit.php
@@ -1,4 +1,5 @@
 <?php
+
 include('config1.php');
 include('services/userdataservice.php');
 include('services/searchdataservice.php');
@@ -213,74 +214,4 @@ else if($_REQUEST['act'] == 'edit_user_profile')
 }
 
 
-
-
-/*****************************Create My Diet Plan**************************/
-
-else if($_REQUEST['act'] == 'my_diet_plan')
-{
-  $userdata       =   (file_get_contents("php://input"));
-  $userid         =   $_REQUEST['userid'];
-  $req            =   new UserProfileService();
-  $res            =   $req->diet_plan($userdata,$userid);
-  if($res)
-        {
-          $data = array('status' => '1', 'data'=> $res, 'msg'=>'Create diet plan');
-                  echo json_encode($data);
-        }
-        else
-        {
-            $data = array('status' => '0', 'data'=>$res, 'msg'=>'not created');
-                  echo json_encode($data);
-        }   
-
-}
-
-
-
-
-/***********************Listing Diet Plan********************************/
-
-
-
-else if($_REQUEST['act'] == 'list_diet_plan')
-{
- $userid         =   $_REQUEST['userid'];
- $req            =   new UserProfileService();
- $res            =   $req->list_plan($userid);
-  if($res)
-        {
-          $data = array('status' => '1', 'data'=> $res, 'msg'=>'List diet plan');
-                  echo json_encode($data);
-        }
-        else
-        {
-            $data = array('status' => '0', 'data'=>[], 'msg'=>'not list');
-                  echo json_encode($data);
-        }   
-
-}
-
-/***********************Listing Diet Plan********************************/
-
-
-else if($_REQUEST['act'] == 'edit_diet_plan')
-{
-  $my_diet_plan   =   (file_get_contents("php://input"));
-  $id             =   $_REQUEST['id'];
-  $req            =   new UserProfileService();
-  $res            =   $req->edit_plan($id,$my_diet_plan);
-  if($res)
-  {
-      $data = array('status' => '1', 'data'=> "$res", 'msg'=>'Updated diet plan');
-                  echo json_encode($data);
-        }
-        else
-        {
-            $data = array('status' => '0', 'data'=>"0", 'msg'=>'not updated diet plan');
-                  echo json_encode($data);
-        }   
-
-}
-
-
+?>
