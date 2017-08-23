e2e1a46b195b795bb8dd8f67b04d36af0199cd08
dev538623 <devendra@darkhorsesports.in>
2017-08-23 17:50:09 +0530

    diet plan

--- dietPlanController.php
+++ dietPlanController.php
@@ -1,6 +1,6 @@
 <?php
 include('config1.php');
-include('services/MydietPlan.php');
+include('services/MydietPlanService.php');
 
 /*****************************Create My Diet Plan**************************/
 
@@ -8,7 +8,7 @@ if($_REQUEST['act'] == 'my_diet_plan')
 {
   $userdata       =   (file_get_contents("php://input"));
   $userid         =   $_REQUEST['userid'];
-  $req            =   new MydietPlan();
+  $req            =   new MydietPlanService();
   $res            =   $req->diet_plan($userdata,$userid);
   if($res)
         {
@@ -33,7 +33,7 @@ if($_REQUEST['act'] == 'my_diet_plan')
 else if($_REQUEST['act'] == 'list_diet_plan')
 {
  $userid         =   $_REQUEST['userid'];
- $req            =   new MydietPlan();
+ $req            =   new MydietPlanService();
  $res            =   $req->list_plan($userid);
   if($res)
         {
@@ -56,12 +56,12 @@ else if($_REQUEST['act'] == 'edit_diet_plan')
 {
   $my_diet_plan   =   (file_get_contents("php://input"));
   $id             =   $_REQUEST['id'];
-  $req            =   new MydietPlan();
+  $req            =   new MydietPlanService();
   $res            =   $req->edit_plan($id,$my_diet_plan);
-  if($res)
-  {
-      $data = array('status' => '1', 'data'=> "$res", 'msg'=>'Updated diet plan');
-                  echo json_encode($data);
+        if($res)
+        {
+            $data = array('status' => '1', 'data'=> "$res", 'msg'=>'Updated diet plan');
+                        echo json_encode($data);
         }
         else
         {
@@ -70,6 +70,31 @@ else if($_REQUEST['act'] == 'edit_diet_plan')
         }   
 
 }
+
+
+else if ($_REQUEST['act']=='find_diet_log') 
+{
+  $userid            =   $_REQUEST['userid'];
+  $req               =   new MydietPlanService();
+  $res               =   $req->list_ashin_log($userid); 
+
+  if($res)
+  {
+    $data = array('status' => '0', 'data'=>$res, 'msg'=>'ashin diet plan log');
+                  echo json_encode($data);
+  }
+  else
+  {
+    $data = array('status' => '0', 'data'=>"0", 'msg'=>'not ashin diet plan log');
+                  echo json_encode($data);
+  }
+
+
+  
+}
+
+
+
 
 
 
