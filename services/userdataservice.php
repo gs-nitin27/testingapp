b3c630f7c2e74c3c5a82cc523be3dcbc21dd253a
dev538623 <devendra@darkhorsesports.in>
2017-09-19 18:27:40 +0530

    bug fix

--- services/userdataservice.php
+++ services/userdataservice.php
@@ -704,32 +704,19 @@ $query = mysql_query("SELECT `id`, IFNull(`userid`,'') AS userid, IFNull(`title`
 else if ($type == 2) 
 {
 
-$query = mysql_query("SELECT `id`, IFNull(`userid`,'') AS userid,IFNull(`image`,'') AS image, IFNull(`type`,'') AS type, IFNull(`feetype`,'') AS feetype, IFNull(`name`,'') AS name, IFNull(`address_1`,'') AS address_1, IFNull(`address_2`,'') AS address_2, IFNull(`location`,'') AS location, IFNull(`PIN`,'') AS PIN, IFNull(`state`,'') AS state, IFNull(`description`,'') AS description, IFNull(`sport`,'') AS sport, IFNull(`ticket_detail`,'') AS ticket_detail,IFNull(`no_of_ticket`,'') AS no_of_ticket, IFNull(`eligibility1`,'') AS eligibility, IFNull(`terms_cond1`,'') AS terms_cond, IFNull(`organizer_name`,'') AS organizer_name, IFNull(`mobile`,'') AS mobile,IFNull(`organizer_address_line1`,'') AS organizer_address_line1, IFNull(`organizer_address_line2`,'') AS organizer_address_line2, IFNull(`organizer_city`,'') AS organizer_city, IFNull(`organizer_state`,'') AS organizer_state, IFNull(`organizer_pin`,'') AS organizer_pin, IFNull(`event_links`,'') AS event_links, IFNull(DATE_FORMAT(`start_date`, '%D %M %Y'),'') AS start_date, IFNull(DATE_FORMAT(`end_date`, '%D %M %Y'),'') AS end_date, IFNull(DATE_FORMAT(`entry_start_date`, '%D %M %Y'),'') AS entry_start_date, IFNull(DATE_FORMAT(`entry_end_date`, '%D %M %Y'),'') AS entry_end_date, IFNull(`file_name`,'') AS file_name, IFNull(`file`,'') AS file, IFNull(`email_app_collection`,'') AS email_app_collection, IFNull(DATE_FORMAT(`dateCreated`, '%D %M %Y'),'') AS dateCreated,IFNull(DATEDIFF(`entry_start_date`,CURDATE()) , '') AS days,IFNull(DATEDIFF(`entry_end_date`,CURDATE()) , '') AS open , IFNull(`publish`,'') AS publish FROM `gs_eventinfo` WHERE ".$where." ORDER BY `id` DESC ");
+$query = mysql_query("SELECT `id`, IFNull(`userid`,'') AS userid,IFNull(`image`,'') AS image, IFNull(`type`,'') AS type, IFNull(`feetype`,'') AS feetype, IFNull(`name`,'') AS name, IFNull(`address_1`,'') AS address_1, IFNull(`address_2`,'') AS address_2, IFNull(`location`,'') AS location, IFNull(`PIN`,'') AS PIN, IFNull(`state`,'') AS state, IFNull(`description`,'') AS description, IFNull(`sport`,'') AS sport, IFNull(`ticket_detail`,'') AS ticket_detail,IFNull(`no_of_ticket`,'') AS no_of_ticket, IFNull(`eligibility1`,'') AS eligibility, IFNull(`terms_cond1`,'') AS terms_cond, IFNull(`organizer_name`,'') AS organizer_name, IFNull(`mobile`,'') AS mobile,IFNull(`organizer_address_line1`,'') AS organizer_address_line1, IFNull(`organizer_address_line2`,'') AS organizer_address_line2, IFNull(`organizer_city`,'') AS organizer_city, IFNull(`organizer_state`,'') AS organizer_state, IFNull(`organizer_pin`,'') AS organizer_pin, IFNull(`event_links`,'') AS event_links, IFNull(`start_date`, '') AS start_date, IFNull(`end_date`, '') AS end_date, IFNull(`entry_start_date`, '') AS entry_start_date, IFNull(`entry_end_date`, '') AS entry_end_date, IFNull(`file_name`,'') AS file_name, IFNull(`file`,'') AS file, IFNull(`email_app_collection`,'') AS email_app_collection, IFNull(`dateCreated`, '') AS dateCreated,IFNull(DATEDIFF(`entry_start_date`,CURDATE()) , '') AS days,IFNull(DATEDIFF(`entry_end_date`,CURDATE()) , '') AS open , IFNull(`publish`,'') AS publish FROM `gs_eventinfo` WHERE ".$where." ORDER BY `id` DESC ");
 }
 
 else if($type == 3)
 {
-
-
 $query = mysql_query("SELECT `id`, IFNull(`userid`,'') AS userid, IFNull(`name`,'')AS name, IFNull(`address_1`,'') AS address_1
 
-
 , IFNull(`address_2`,'') AS address_2, IFNull(`location`,'') AS location, IFNull(`state`,'') AS state
-
-
 , IFNull(`pin`,'') AS pin, IFNull(`description`,'') AS description, IFNull(`sport`,'') AS sport, IFNull
-
-
 (`level`,'') AS level, IFNull(`age_group`,'') AS age_group, IFNull(`gender`,'') AS gender, IFNull(`terms_and_cond1`,'') AS terms_cond , IFNull(`terms_and_cond2`,'') AS terms_and_cond2, IFNull(`organiser_name`,'') AS organiser_name, IFNull(`mobile`,'') AS mobile,IFNull(`eligibility1`, '') AS eligibility
-
-
 , IFNull(`landline`,'') AS landline, IFNull(`email`,'') AS email, IFNull(`org_address1`,'') AS org_address1
-
-
 , IFNull(`org_address2`,'') AS org_address2, IFNull(`org_city`,'')AS org_city , IFNull(`org_pin`,'')
-
-
- AS org_pin , IFNull(`tournaments_link`,'') AS tournaments_link, IFNull(DATE_FORMAT(`start_date`, '%D %M %Y'),'') AS start_date, IFNull(DATE_FORMAT(`end_date`, '%D %M %Y'),'') AS end_date, IFNull(DATE_FORMAT(`event_entry_date`, '%D %M %Y'),'') AS event_entry_date, IFNull(DATE_FORMAT(`event_end_date`, '%D %M %Y'),'') AS event_end_date, IFNull(`file_name`,'') AS file_name, IFNull(`file`,'') AS file, IFNull(`image`,'') AS image, IFNull(`email_app_collection`,'') AS email_app_collection,IFNull(`phone_app_collection`,'') AS phone_app_collection , IFNull(DATEDIFF(`event_entry_date`,CURDATE()) , '') AS days , IFNull(DATEDIFF(`event_end_date`,CURDATE()) , '') AS open ,IFNull(DATE_FORMAT(`date_created`, '%D %M %Y'),'') AS date_created  , IFNull(`publish`,'') AS publish   FROM `gs_tournament_info`  WHERE ".$where."  ORDER BY `date_created` ASC");
+ AS org_pin , IFNull(`tournaments_link`,'') AS tournaments_link, IFNull(`start_date`, '') AS start_date, IFNull(`end_date`, '') AS end_date, IFNull(`event_entry_date`, '') AS event_entry_date, IFNull(DATE_FORMAT(`event_end_date`, '%D %M %Y'),'') AS event_end_date, IFNull(`file_name`,'') AS file_name, IFNull(`file`,'') AS file, IFNull(`image`,'') AS image, IFNull(`email_app_collection`,'') AS email_app_collection,IFNull(`phone_app_collection`,'') AS phone_app_collection , IFNull(DATEDIFF(`event_entry_date`,CURDATE()) , '') AS days , IFNull(DATEDIFF(`event_end_date`,CURDATE()) , '') AS open ,IFNull(`date_created`, '') AS date_created  , IFNull(`publish`,'') AS publish   FROM `gs_tournament_info`  WHERE ".$where."  ORDER BY `date_created` ASC");
 
 
 }
