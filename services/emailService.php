3cff42c9a67fa623de9d24070e5ed63090789098
nitn <nitin@darkhorsesports.in>
2017-12-28 12:57:05 +0530

    bug fixing

--- services/emailService.php
+++ services/emailService.php
@@ -463,8 +463,13 @@ public function contact_coach($data)
          $mail->Username =$from;  
          $mail->Password = "2016Darkhorse";
          $mail->SetFrom($from, $from_name);
-         $mail->Subject = $data->athlete_name." wants to join your class";
-         $mail->Body = "hello ".$data->coach_name."<br><br>"." an aspiring athlete has sent you a message:"."<br><br>".$data->message."<br>You can view his clicking on the link:<br><a href='http://getsporty.in/profile/index.php?userid=".$data->userid."&prof_id=1'>View Profile</a><br><br>"."Thanks"."<br>"."Team Getsporty"; 
+         if($data->prof_id == '1')
+         {$mail->Subject = $data->athlete_name." wants to join your class";}
+         else
+         {
+          $mail->Subject = $data->prof_name.'  '.$data->athlete_name." has sent you a message";
+         }
+         $mail->Body = "hello ".$data->coach_name."<br><br>"." a ".$data->prof_name." has sent you a message:"."<br><br>".$data->message."<br>You can view his profile , clicking on the view profile link:<br><a href='http://getsporty.in/profile/index.php?userid=".$data->userid."&prof_id=1'>View Profile</a><br><br>"."Thanks"."<br>"."Team Getsporty"; 
                $txt='This email was sent in HTML format. Please make sure your preferences allow you to view HTML emails.'; 
                $mail->AltBody = $txt; 
                $mail->AddAddress($to);
