0d06346abe7548505b57f1cd1af37af95277d012
nitn <nitin@darkhorsesports.in>
2018-03-16 14:07:28 +0530

    api commit

--- angularapi_image.php
+++ angularapi_image.php
@@ -16,9 +16,10 @@ echo json_encode($imageName);
 }
 
 else if($_REQUEST['act'] == 'jobimage')
-{   
+{   error_reporting(E_ERROR | E_WARNING | E_PARSE);
 ini_set('memory_limit', '-1');	
 $data =  file_get_contents("php://input");
+$data = preg_replace('#^data:image/[^;]+;base64,#', '', $data);
 $imageData = base64_decode($data);
 $source = imagecreatefromstring($imageData);
 $angle = 0;
@@ -26,8 +27,23 @@ $imageName = 'job_'.time().'.jpeg';
 $rotate = imagerotate($source, $angle, 0); 
 $imageSave = imagejpeg($rotate,$imageName,100);
 $newpath = UPLOAD_DIR_JOB.$imageName;
-rename($imageName,$newpath);
-echo json_encode($imageName);
+if(isset($_REQUEST['ui']))
+{
+$resp = rename($imageName,$newpath);
+//echo json_encode($imageName);
+if($resp)
+{
+	$response  = array('status' =>'1' ,'data'=>$imageName );
+}else
+{
+	$response  = array('status' =>'0' ,'data'=>[] );
+}
+echo json_encode($response);
+
+}else
+{
+echo json_encode($imageName);	
+}
 }
 
 else if($_REQUEST['act'] == 'profilejob')
