dce2db192fc004744b59a61f62ca5dc6b693b153
sagar seth <sagar12jan@gmail.com>
2018-05-02 16:47:29 +0530

    bug fixing in subscribr api

--- cron/visitor_cron_controller.php
+++ cron/visitor_cron_controller.php
@@ -10,13 +10,30 @@ $obj_var = $obj->get_subscriber_data();
 $emailObj = new emailService();
 
 if($obj_var != 0)
-{
+{  $filterArray = array_filter($obj_var);
+
+if(!count($filterArray) == 0){
+//    return false;
    foreach ($obj_var as $key => $value) {
    	   if($value != null)
    	   {
    	   	foreach ($value as $key1 => $value1) {
+   	   	 //  echo $value1['unique_code'];die;
+
    	   		$resp  = $emailObj->create_subscribe_body($value1,$key);
-   	   	}
+   	   	    if($resp != 0)
+   	   	    {   //echo $resp.'-------'.$value1['unique_code'];die;
+   	   	    	$update = $obj->update_sent_item($resp,$value1['unique_code'],$key);
+   	   	    	if($update == '1')
+   	   	    	{
+   	   	    		$response = array('status' =>'1' , 'message'=>'Success','data'=>$value1['unique_code']);
+   	   	    	}else
+   	   	    	{
+   	   	    		$response = array('status' =>'0' , 'message'=>'Failure','data'=>$value1['unique_code']);
+   	   	    	}
+   	   	    echo json_encode($response);
+   	   	    }
+    	   	}
    	   }
    }
 
@@ -36,7 +53,10 @@ if($obj_var != 0)
 	// {
 	// 	echo "oh! no!";
 	// }
+}else{
+	$resp = array('status' =>'0' ,'msg'=>'No records Found' );
+	echo json_encode($resp);
 }
-
+}
 
  ?>
--- services/cron_service/visitor_cron_service.php
+++ services/cron_service/visitor_cron_service.php
@@ -3,57 +3,70 @@ class visitor_cron_service
 {
  public function get_subscriber_data()
 	{
-      $query = mysql_query("SELECT `email`,`where`,`phone`,`module` ,`sport`FROM `visitor_subscribe` WHERE '1=1'");
+      $query = mysql_query("SELECT `email`,`where`,`phone`,`module` ,`sport`,`unique_code`,`sent_items`FROM `visitor_subscribe` WHERE '1=1'");
       if(mysql_num_rows($query)>0)
       {
       	while ($row = mysql_fetch_assoc($query)) {
-      		
-      		if($row['module'] == '1')
+      	
+          $row['sent_items'];
+      		if($row['sent_items'] != '')
+          {
+            $sent_items = $row['sent_items'];   
+          }else
+          {
+            $sent_items = "'"."'";
+          }
+          if($row['module'] == '1')
       		{     $sport = $row['sport'];
-                $where =  $row['where']." `sport` LIKE '%$sport%'";
+                $where = $row['where']." `sport` LIKE '%$sport%' AND `id` NOT IN ($sent_items)";
                 $job_data =  $this->getJobData($where);
       	        if($job_data != 0)
       	        {
                 $job[$row['phone']]['data'][] = $job_data;  //get job data to send inmail notification
-      		    	$job[$row['phone']]['user_info'] = $row['email'];	
+      		    	$job[$row['phone']]['user_info'] = $row['email'];
+                $job[$row['phone']]['unique_code'] = $row['unique_code'];	
       	        }
       		}
       		if($row['module'] == '2')
       		{    $sport = $row['sport'];
-               $where =  $row['where']."  `sport_name` LIKE '%$sport%'";
+               $where =  $row['where']."  `sport_name` LIKE '%$sport%' AND `id` NOT IN ($sent_items)";
                $event_data =  $this->getEventData($where);
                 if($event_data != 0)
                 {
                 $event[$row['phone']]['data'][] = $event_data;   //get event data to send inmail notification
                 $event[$row['phone']]['user_info'] = $row['email'];
+                $event[$row['phone']]['unique_code'] = $row['unique_code'];
                 }
       		}if($row['module'] == '3')
       		{    $sport = $row['sport'];
-               $where =  $row['where']."  `sport` LIKE '%$sport%'";
+               $where =  $row['where']."  `sport` LIKE '%$sport%' AND `id` NOT IN ('$sent_items')";
                 $tournament_data =  $this->getTournamentData($where);
                 if($tournament_data != 0)
                 {
                 $tournament[$row['phone']]['data'][] =  $tournament_data;  //get tournament data to send inmail notification
                 $tournament[$row['phone']]['user_info'] = $row['email'];
+                $tournament[$row['phone']]['unique_code'] = $row['unique_code'];
                 }
       		}if($row['module'] == '4')
       		{     $sport = $row['sport'];
-                $where =  $row['where']."  `sport` LIKE '%$sport%'";
+                $where =  $row['where']."  `sport` LIKE '%$sport%' AND `id` NOT IN ('$sent_items')";
                 $trial_data =  $this->getEventData($where);
                 if($trial_data != 0)
                 {
 
                 $trial[$row['phone']]['data'][] = $trial_data;     //get trial data to send inmail notification
                 $trial[$row['phone']]['user_info'] = $row['email'];
+                $trial[$row['phone']]['unique_code'] = $row['unique_code'];
                 }        		
           }if($row['module'] == '6')
       		{     $sport = $row['sport'];
-                $where =  $row['where']."  `sport` LIKE '%$sport%'";
+                $where =  $row['where']."  `sport` LIKE '%$sport%' AND `id` NOT IN ('$sent_items')";
       			    $article_data =  $this->getArticleData($where);
                 if( $article_data != 0)
                 {
                 $article[$row['phone']]['data'][] = $article_data; //get article data to send inmail notification
                 $article[$row['phone']]['user_info'] = $row['email'];
+                $article[$row['phone']]['unique_code'] = $row['unique_code'];
       		      }
           }
       		
@@ -67,7 +80,7 @@ class visitor_cron_service
 	}
  public function getJobData($where)
  {  
-    $query = mysql_query("SELECT `id`,`title`,`description`,`image` FROM `gs_jobInfo` WHERE ".$where." AND datediff(CURDATE(), `date_updated`) < 7");
+  $query = mysql_query("SELECT `id`,`title`,`description`,`image` FROM `gs_jobInfo` WHERE ".$where." AND datediff(CURDATE(), `date_updated`) < 30 ");
     if(mysql_num_rows($query)>0)
     {
      while ($row = mysql_fetch_assoc($query)) {
@@ -83,7 +96,7 @@ class visitor_cron_service
 
  public function getArticleData($where)
  {
-    $query = mysql_query("SELECT  `id`,`title`,`description`,`image` FROM `gs_resources` WHERE ".$where."");
+    $query = mysql_query("SELECT  `id`,`title`,`description`,`image`,`summary` FROM `gs_resources` WHERE ".$where."");
     if(mysql_num_rows($query)>0)
     {
      while ($row = mysql_fetch_assoc($query)) {
@@ -98,7 +111,7 @@ class visitor_cron_service
  }
 
  public function getEventData($where)
- {  
+ { //echo "SELECT `id`,`name`,`description`,`image` FROM `gs_eventinfo` WHERE ".$where." AND datediff(CURDATE(), `end_date`) < 0 AND `publish` = '1' ";die;
     $query = mysql_query("SELECT `id`,`name`,`description`,`image` FROM `gs_eventinfo` WHERE ".$where." AND datediff(CURDATE(), `end_date`) < 0 AND `publish` = '1' ");
     if(mysql_num_rows($query)>0)
     {
@@ -115,7 +128,7 @@ class visitor_cron_service
 
 public function getTournamentData($where)
  {
-  $query = mysql_query("SELECT `id`,`name`,`description`,`image` FROM `gs_tournament_info` WHERE ".$where." AND datediff(CURDATE(), `end_date`) < 0");
+  $query = mysql_query("SELECT `id`,`name`,`description`,`image` FROM `gs_tournament_info` WHERE ".$where." AND datediff(CURDATE(), `end_date`) < 0 ");
     if(mysql_num_rows($query)>0)
     {
      while ($row = mysql_fetch_assoc($query)) {
@@ -129,7 +142,19 @@ public function getTournamentData($where)
     }
  }
 
- 
+ public function update_sent_item($item,$key)
+ {
+//echo "UPDATE `visitor_subscribe` SET `sent_items`  = '$item' WHERE `unique_code` = '$key' ";die;
+ $query = mysql_query("UPDATE `visitor_subscribe` SET `sent_items`  = '$item' WHERE `unique_code` = '$key' ");
+ if($query)
+ {
+  return 1;
+ }
+else
+{
+  return 0;
+}
+}
 
 }
  ?>
